5ca4c8cf358fde34aada67ae524291df9444d2f8
Ilker Ismail Cakir <ilker.cakir13@gmail.com>
2017-05-27 16:54:08 +0300

    Add files via upload

--- v4l2grab.c
+++ v4l2grab.c
@@ -22,8 +22,8 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-// compile with gcc -Wall -c "%f" -DIO_READ -DIO_MMAP -DIO_USERPTR
-// link with gcc -Wall -o "%e" "%f" -ljpeg -DIO_READ -DIO_MMAP -DIO_USERPTR
+// compile with gcc -Wall -c "%f" -DUSE_OPENGL -DUSE_EGL -DIS_RPI -DSTANDALONE -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS -DTARGET_POSIX -D_LINUX -fPIC -DPIC -D_REENTRANT -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -U_FORTIFY_SOURCE -g -ftree-vectorize -pipe -DHAVE_LIBBCM_HOST -DUSE_EXTERNAL_LIBBCM_HOST -DUSE_VCHIQ_ARM -I/opt/vc/include/ -I/opt/vc/include/interface/vcos/pthreads -I/opt/vc/include/interface/vmcs_host/linux -Wno-deprecated-declarations -DIO_READ -DIO_MMAP -DIO_USERPTR $(pkg-config --cflags gtk+-3.0)
+// link with gcc -Wall -o "%e" "%f" -D_POSIX_C_SOURCE=199309L -DIO_READ -DIO_MMAP -DIO_USERPTR $(pkg-config --cflags gtk+-3.0) -Wl,--whole-archive -I/opt/vc/include -L/opt/vc/lib/ -lGLESv2 -lEGL -lbcm_host -lvchiq_arm -lpthread -lrt -ldl -lm -Wl,--no-whole-archive -rdynamic $(pkg-config --libs gtk+-3.0) $(pkg-config --cflags gtk+-3.0) $(pkg-config --libs gtk+-3.0) -ljpeg -lm
 
 #define _GNU_SOURCE
 
